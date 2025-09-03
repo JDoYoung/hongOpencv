2df56ff20c5996cfb7a651ebb98153674359d4fb
JDoYoung <airwalk6346@gmail.com>
2025-09-03 16:28:46 +0900

    feat: camera test파일 추가

--- python/a09_camera.py
+++ python/a09_camera.py
@@ -15,7 +15,7 @@ def put_string(frame, text, pt, value, color=(120, 200, 90)):             # 문�
     cv2.putText(frame, text, pt, font, 0.7, (120, 200, 90), 2)  # 글자 적기
 
 def main():
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture("/home/aa/hongOpencv/data/vtest.avi")
 
     print(f"너비 {cap.get(cv2.CAP_PROP_FRAME_WIDTH)}")
     print(f"높이 {cap.get(cv2.CAP_PROP_FRAME_HEIGHT)}")
